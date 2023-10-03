c10b7f1f532901559abb2de952ca57efe10ed796
IamCat <96004821+PerthTheIamCat@users.noreply.github.com>
2023-10-03 22:13:52 +0700

    Add files via upload

---
+++ Lab5/p55/Lab55Block.java
@@ -0,0 +1,10 @@
+package p55;
+
+public class Lab55Block {
+    public static void main(String[] args) {
+        M obj = new M();
+    }
+    static{
+        System.out.println("Lab56Block's static block");
+    }
+}
---
+++ Lab5/p55/N.java
@@ -0,0 +1,14 @@
+package p55;
+
+class N {
+    N(){
+        System.out.println("N's constructor body");
+    }
+    {
+        System.out.println("N's instance initialization block");
+    }
+
+    static{
+        System.out.println("N's static initialization block");
+    }
+}
